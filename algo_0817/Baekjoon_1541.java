f7bea304662b250f241f3c7d484f71ec410c5f91
jun <ktf1406@naver.com>
2022-08-18 14:20:46 +0900

    2022.08.17 algo study code

---
+++ algo_0817/Baekjoon_1541.java
@@ -0,0 +1,17 @@
+package algo_0817;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Baekjoon_1541 {
+	// ASCII + 43 - 45
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str = br.readLine();
+		
+		for(int i = 0; i<str.length(); i++) {
+			char c = str.charAt(i);
+		}
+	}
+}
